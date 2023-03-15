7ad17f0807c5df9c01128792c43a9f238ca272c5
Behruzsv <126698226+Behruzsv@users.noreply.github.com>
2023-03-15 22:07:03 +0300

    15-03-2023 model binding deyiz

---
+++ Controllers/MVC04ModelBindingController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+	public class MVC04ModelBindingController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult KullaniciDetay()
+		{
+			Kullanici kullanici = new();
+			kullanici.KullaniciAdi = "Admin";
+			kullanici.Ad = "Murat";
+            kullanici.Soyad = "Yılmaz";
+            kullanici.Sifre = "123";
+            kullanici.Email = "Murat@yilmaz.co";
+            return View(kullanici); // View içerisinde model datası olarak kullanıcı nesnesini sayfaya gönderdik
+		}
+		[HttpPost]
+		public IActionResult KullaniciDetay(Kullanici kullanici) // post metodunda modelden gelen nesneyi bu şekilde parantez içinde yakalayabiliyoruz
+		{
+			return View(kullanici); // gelen kullanıcı nesnesini tekrardan ekrana yolla
+		}
+		public IActionResult AdresDetay()
+		{
+			Adres adres = new()
+			{
+				Sehir = "Çankırı", Ilce = "Çerkeş", AcikAdres = "1907 sk. Mo : 21 A"
+			};
+			return View(adres);
+		}
+		public IActionResult UyeSayfasi()
+		{
+			return View();
+		}
+	}
+}
